0b0166b91fae487a62a2fa444a3c1e5a2979ac32
NTB <digestivbiscuitsm@gmail.com>
2021-06-30 11:36:55 +0100

    Added 2 Python files

---
+++ python/school/nestediteration.py
@@ -0,0 +1,7 @@
+num = int(input("Enter number: "))
+total = 0
+while total < 100:
+    for i in range(1, 3):
+        total = total + num
+    print (total)
+
---
+++ python/school/nestedselection.py
@@ -0,0 +1,5 @@
+num = int(input("Enter number: "))
+total = 0
+if num > 10:
+    print("Too high")
+    
